421f5c60287add3ea0663fe3574f28dc8db7444e
Bryle Patalinghug <brylemp@yahoo.com>
2019-10-04 19:31:09 +0800

    Added LE32

---
+++ LE32/LE32/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  LE32
+//
+//  Created by Bryle Patalinghug on 04/10/2019.
+//  Copyright © 2019 Bryle Patalinghug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *quote;
+@property (weak, nonatomic) IBOutlet UILabel *num;
+@property (weak, nonatomic) IBOutlet UIButton *Gen;
+@property int oldq;
+
+
+
+@end
+
