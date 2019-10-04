5bd53c5f1ae7774b81b6b93ba773a10009320de6
Bryle Patalinghug <brylemp@yahoo.com>
2019-10-04 21:27:50 +0800

    Finished LE33

--- LE33/LE33/ViewController.m
+++ LE33/LE33/ViewController.m
@@ -26,18 +26,20 @@
     
     if(res < 18.5){
         _BMItext.text = @"Underweight";
-        _BMIimage.image = [UIImage s]
+        _BMIimage.image = [UIImage imageNamed:@"uw.png"];
     }
     else if(res >= 18.5 && res <= 24.9){
         _BMItext.text = @"Normal";
+        _BMIimage.image = [UIImage imageNamed:@"n.png"];
     }
     else if(res >= 25 && res <= 30){
         _BMItext.text = @"Overweight";
+        _BMIimage.image = [UIImage imageNamed:@"ow.png"];
     }
     else{
         _BMItext.text = @"Obese";
+        _BMIimage.image = [UIImage imageNamed:@"ob.png"];
     }
-    
     _BMIfactor.text = [NSString stringWithFormat:@"%.2f", res];
 }
 
