1dec6a275c578030c66a245da9beaf7e66712ba6
PATALINGHUG BRYLE <patalinghugbr_cpe76@CEAC-TCs-iMac-47.local>
2019-11-16 11:04:24 +0800

    Added empty proj for le4

---
+++ LE42x8/LE42x8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LE42x8
+//
+//  Created by PATALINGHUG BRYLE on 16/11/2019.
+//  Copyright © 2019 PATALINGHUG BRYLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LE42x8/LE42x8Tests/LE42x8Tests.m
@@ -0,0 +1,39 @@
+//
+//  LE42x8Tests.m
+//  LE42x8Tests
+//
+//  Created by PATALINGHUG BRYLE on 16/11/2019.
+//  Copyright © 2019 PATALINGHUG BRYLE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LE42x8Tests : XCTestCase
+
+@end
+
+@implementation LE42x8Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
